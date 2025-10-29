085393dfe444e280fb8971b3e23b463e7672b002
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2025-10-29 15:51:14 +0100

    remove opengl from project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# atc_opengl
+# atc_imgui
 Air Traffic Controller simulator for school project, written primarily in C++. Co-authored /w @NatanKwaskiewicz &amp; @Komputerr
+
+Based. Based in ImGUI and written by goats.
--- main.cpp
+++ main.cpp
@@ -1,39 +1,5 @@
-#define GLFW_INCLUDE_NONE
-
-#include <GLFW/glfw3.h>
-#include <glad/glad.h>
-#include <iostream>
-
+#include<iostream>
+using namespace std;
 int main() {
-    if (!glfwInit()) {
-        std::cout << "Error in initalizing glfw\n";
-        return -1;
-    }
-
-    GLFWwindow *window =
-        glfwCreateWindow(400, 400, "OpenGL Tutorial", NULL, NULL);
-
-    if (!window) {
-        std::cout << "Failed to create window\n";
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGL()) {
-        std::cout << "Failed to initalize glad\n";
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
-
-    std::cout << "Opengl Version\t" << GLVersion.major << "." << GLVersion.minor
-              << std::endl;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+    cout<<"1 i love everyone especially the worse";
 }
