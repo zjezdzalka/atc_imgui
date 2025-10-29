e73859db3b21f108e54bb2bf0c0ae7a67a493db3
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2025-10-29 14:46:54 +0100

    it works!!!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        std::cout << "Error in initalizing glfw\n";
+        return -1;
+    }
+
+    GLFWwindow *window =
+        glfwCreateWindow(400, 400, "OpenGL Tutorial", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL()) {
+        std::cout << "Failed to initalize glad\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    std::cout << "Opengl Version\t" << GLVersion.major << "." << GLVersion.minor
+              << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
